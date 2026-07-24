07ff7297574c7d8cb0f92b411573b622eacd3b1f
laporejero <laporejero1297@gmail.com>
2026-07-24 16:56:37 +0800

    completed Exercise 5. WebBmi

--- healthapp/bmiCalculator.ts
+++ healthapp/bmiCalculator.ts
@@ -1,4 +1,4 @@
-function calculateBmi(heightCm: number, weightKg: number) {
+export function calculateBmi(heightCm: number, weightKg: number) {
     const heightM: number = heightCm / 100;
     const bmi: number = weightKg / Math.pow(heightM, 2);
     let category: string = ""
@@ -16,18 +16,18 @@ function calculateBmi(heightCm: number, weightKg: number) {
     return `${category} range`
 }
 
-// console.log(calculateBmi(180, 74))
+if (process.argv[1] === import.meta.filename) {
+    const bmiArgs: string[] = process.argv.slice(2);
 
-const bmiArgs: string[] = process.argv.slice(2);
+    if (bmiArgs.length < 2) {
+    console.log('Error: Please provide a target value followed by daily exercise hours.');
+    console.log('Example: npm run calculateBMI 180 74');
+    process.exit(1);
+    }
 
-if (bmiArgs.length < 2) {
-  console.log('Error: Please provide a target value followed by daily exercise hours.');
-  console.log('Example: npm run calculateBMI 180 74');
-  process.exit(1);
+    const heightCm = Number(bmiArgs[0]);
+
+    const weightKg = Number(bmiArgs[1]);
+
+    console.log(calculateBmi(heightCm, weightKg));
 }
-
-const heightCm = Number(bmiArgs[0]);
-
-const weightKg = Number(bmiArgs[1]);
-
-console.log(calculateBmi(heightCm, weightKg));
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,9 +1,35 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator.ts';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+
+    if (
+        !req.query.height ||
+        !req.query.weight ||
+        isNaN(height) ||
+        isNaN(weight)
+    ) {
+        return res.status(400).json({ 
+            error: "malformatted parameters"
+        });
+    }
+
+    const bmi = calculateBmi(height, weight);
+
+    return res.json({
+        height: height,
+        weight: weight,
+        bmi: bmi
+    });
+})
 
 const PORT = 3003;
 
