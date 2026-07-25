c793f40c20eb3e4d49b54395a080e925b654cb54
laporejero <laporejero1297@gmail.com>
2026-07-25 15:35:24 +0800

    updated to PORT 3000

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -63,7 +63,7 @@ app.post('/exercises', (req, res) => {
     return res.json(calcExercise);
 });
 
-const PORT = 3003;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
