698e145ce890306aec79b559d53cbd4492a19c80
laporejero <laporejero1297@gmail.com>
2026-07-27 16:40:28 +0800

    Implementing the functionality

--- flightdiaries/backend/src/services/diaryService.ts
+++ flightdiaries/backend/src/services/diaryService.ts
@@ -1,39 +1,14 @@
-import diaryEntries from '../../data/entries.ts';
-import type { DiaryEntry, NonSensitiveDiaryEntry, NewDiaryEntry } from '../types.ts';
+import diaryData from '../../data/entries.json' with { type: "json" };
 
-const diaries = diaryEntries;
-
-const getEntries = (): DiaryEntry[]  => {
-  return diaries;
+const getEntries = () => {
+  return diaryData;
 };
 
-const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {  
-  return diaries.map(({ id, date, weather, visibility }) => ({    
-    id,    
-    date,
-    weather,
-    visibility,
-  }));
-};
-
-const findById = (id: number): DiaryEntry | undefined => {  
-  const entry = diaries.find(d => d.id === id);  
-  return entry;
-};
-
-const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {  
-  const newDiaryEntry = {
-    id: Math.max(...diaries.map(d => d.id)) + 1,
-    ...entry  
-  };
-
-  diaries.push(newDiaryEntry);
-  return newDiaryEntry;
+const addDiary = () => {
+  return null;
 };
 
 export default {
   getEntries,
-  getNonSensitiveEntries,
-  addDiary,
-  findById
+  addDiary
 };
