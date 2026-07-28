6ec3efdc0ae20237b5de2707ea922e7ea1cac80e
laporejero <laporejero1297@gmail.com>
2026-07-28 15:29:38 +0800

    update: added POST route

--- flightdiaries/backend/src/routes/diaries.ts
+++ flightdiaries/backend/src/routes/diaries.ts
@@ -8,8 +8,27 @@ router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries())
 });
 
-router.post('/', (_req, res) => {
-  res.send('add a new diary');
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body;
+
+  const addedEntry = diaryService.addDiary({
+    date,
+    weather,
+    visibility,
+    comment,
+  });
+
+  res.json(addedEntry);
+});
+
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 export default router;
