e062958da0ee625f5dd981a9e74c026ab91cc45d
laporejero <laporejero1297@gmail.com>
2026-08-09 18:13:11 +0800

    added entry data to some patients

--- patientor/backend/data/patients.ts
+++ patientor/backend/data/patients.ts
@@ -31,7 +31,22 @@ const data: Patient[] = [
         "ssn": "300179-77A",
         "gender": "male",
         "occupation": "Cop",
-        "entries": []
+        "entries": [
+            {
+                "id": 'fcd59fa6-c4b4-4fec-ac4d-df4fe1f85f62',
+                "date": '2019-08-05',
+                "type": 'OccupationalHealthcare',
+                "specialist": 'MD House',
+                "employerName": 'HyPD',
+                "diagnosisCodes": ['Z57.1', 'Z74.3', 'M51.2'],
+                "description":
+                    'Patient mistakenly found himself in a nuclear plant waste site without protection gear. Very minor radiation poisoning. ',
+                "sickLeave": {
+                    "startDate": '2019-08-05',
+                    "endDate": '2019-08-28'
+                }
+            }
+        ]
     },
     {
         "id": "d27736ec-f723-11e9-8f0b-362b9e155667",
